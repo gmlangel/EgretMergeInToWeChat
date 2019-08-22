6fd6a36068302e4b776c7a45e5b2b9341b1d2253
guominglong <guominglong@51talk.com>
2019-08-22 17:21:30 +0800

    导入“Egret白鹭引擎相关资源”

---
+++ gameEngine/manifest.js
@@ -0,0 +1,7 @@
+require("./js/egret.min.js")
+require("./js/eui.min.js")
+require("./js/assetsmanager.min.js")
+require("./js/tween.min.js")
+require("./js/main.js")
+require("./js/default.thm.js")
+require("./js/game.min.js")
